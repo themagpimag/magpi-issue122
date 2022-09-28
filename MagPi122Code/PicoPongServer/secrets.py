012ad74460d424297a2522388037bf34340d7e07
robthez <robertzwetsloot@gmail.com>
2022-09-28 12:16:57 +0100

    Removed old files

--- MagPi122Code/PicoPongServer/secrets.py
+++
@@ -1,6 +0,0 @@
-secrets = {
-    'ssid': 'Enter_your_SSID_here',
-    'pw': 'Enter_your_Wi-Fi_password_here',
-    }
-
-# Original Pico W LED web server scripts by Nathen Busler at https://github.com/pi3g/pico-w/tree/main/MicroPython/I%20Pico%20W%20LED%20web%20server
